6e53f5f7bae88ae0c1d0e4985cc68f1879ed9acc
Leo2308 <leo_andres1995@hotmail.es>
2019-05-09 00:00:06 -0500

    CatalogoAndroid

---
+++ catalogoPartesComputadora/app/src/main/java/com/example/proycatalogo/menuComprador.kt
@@ -0,0 +1,15 @@
+package com.example.proycatalogo
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class menuComprador : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_comprador)
+    }
+
+
+}
