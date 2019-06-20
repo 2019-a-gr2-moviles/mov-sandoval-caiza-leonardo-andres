1a56c446576dfb02b03785ebcee7af40082b53cd
Leo2308 <leo_andres1995@hotmail.es>
2019-06-20 15:53:47 -0500

    backend

--- 04-backend/backend-movil/api/models/Usuario.js
+++ 04-backend/backend-movil/api/models/Usuario.js
@@ -8,8 +8,59 @@
 module.exports = {
 
   attributes: {
-
+    nombreAtributo: {
+      type:'string'
+    },
+    nombre: {
+      type: 'string',
+      required: true,
+      minLength: 3,
+      maxLength: 60,
+    },
+    cedula: {
+      type: 'string',
+      required: true,
+      unique: true,
+      minLength: 10,
+      maxLength: 25,
+    },
+    username: {
+      type: 'string',
+      required: true,
+      unique: true
+    },
+    fechaNacimiento: {
+      type: 'string'
+    },
+    sueldo: {
+      type: 'number',
+      min: 100.00,
+      max: 5000,
+      defaultsTo: 100.00
+    },
+    estaCasado: {
+      type: 'boolean',
+      defaultsTo: false
+    },
+    latitudCasa: {
+      type: 'string'
+    },
+    longitudCasa: {
+      type: 'string'
+    },
+    tipoUsuario: {
+      type: 'string',
+      enum: ['normal', 'pendiente', 'premium']
+    },
+    correo: {
+      type: 'string',
+      isEmail: true
+    }
   },
 
 };
 
+
+
+
+// http://localhost:1337/usuario
