90438618dd163e4c1650e8cff0169b3640502f8b
Leo2308 <leo_andres1995@hotmail.es>
2019-06-27 16:02:29 -0500

    Parseo y Request HTTP

---
+++ 03-Android/app/src/main/java/com/example/myapplication/ConexionHttpActivity.kt
@@ -0,0 +1,140 @@
+package com.example.myapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.beust.klaxon.Klaxon
+import java.lang.Exception
+import java.util.*
+
+import com.github.kittinunf.result.Result.*
+import com.github.kittinunf.fuel.httpGet
+
+class ConexionHttpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_conexion_http)
+
+        val json = """
+            [
+                {
+                  "usuariosDeEmpresa": [
+                      {
+                          "createdAt": 1561663617636,
+                          "updatedAt": 1561663617636,
+                          "id": 1,
+                          "nombre": "Leonardo",
+                          "fkEmpresa": 1,
+                      }
+                  ],
+                  "createdAt": 1561663617636,
+                  "updatedAt": 1561663617636,
+                  "id": 1,
+                  "nombre": "Manticore Labs"
+                }
+            ]
+        """.trimIndent()
+/*
+        try {
+            val empresaInstancia = Klaxon()
+                .parse<Empresa>(json)
+
+            Log.i(
+                "http",
+                "Nombre ${empresaInstancia?.nombre}"
+            )
+
+            Log.i(
+                "http",
+                "Id ${empresaInstancia?.id}"
+            )
+
+            if (empresaInstancia != null) {
+                Log.i(
+                    "http",
+                    "Fecha ${Date(empresaInstancia.createdAt)}"
+                )
+            }
+
+
+        } catch (e: Exception) {
+            Log.i(
+                "http",
+                "Error instanciando la empresa"
+            )
+        }
+*/
+
+        try {
+            val empresaInstancia = Klaxon()
+                .parseArray<Empresa>(json)
+            empresaInstancia?.forEach {
+                Log.i(
+                    "http",
+                    "Nombre ${it.nombre}"
+                )
+
+                Log.i(
+                    "http",
+                    "Id ${it.id}"
+                )
+
+                Log.i(
+                    "http",
+                    "Fecha ${it.fechaCreacion}"
+                )
+
+                it.usuariosDeEmpresa.forEach {
+                    Log.i(
+                        "http",
+                        "Nombre ${it.nombre}"
+                    )
+                    Log.i(
+                        "http",
+                        "FK ${it.fkEmpresa}"
+                    )
+                }
+
+            }
+        } catch (e: Exception) {
+            Log.i("http", "${e.message}")
+            Log.i(
+                "http",
+                "Error instanciando la empresa"
+            )
+        }
+
+        val url = "http://172.31.104.115:1337/empresa/1"
+
+        url
+            .httpGet()
+            .responseString { request, response, result ->
+                when (result) {
+                    is Failure -> {
+                        val ex = result.getException()
+                        Log.i("http", "Error: ${ex.message}")
+                    }
+                    is Success -> {
+                        val data = result.get()
+                        Log.i("http", "Data: ${data}")
+
+                        val empresaParseada = Klaxon()
+                            .parse<Empresa>(data)
+                        if (empresaParseada != null) {
+                            Log.i("http"," iiiiiiiiiiiiiiiiiiii ")
+                            Log.i("http","${empresaParseada.nombre} ")
+                            Log.i("http","${empresaParseada.id} ")
+                        }
+
+
+                    }
+                }
+
+            }
+
+    }
+
+}
+
+
