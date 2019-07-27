64fc1fbf3f85744a285a751c4dafe0512811bb2b
Leo2308 <leo_andres1995@hotmail.es>
2019-07-26 23:59:06 -0500

    Menu

--- EXAMEN2B/app/src/main/java/com/example/examen2b/MainActivity.kt
+++ EXAMEN2B/app/src/main/java/com/example/examen2b/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.example.examen2b
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnEntrar.setOnClickListener {
+            irAMenu()
+
+        }
+    }
+
+    fun irAMenu(){
+
+        val intentMenu = Intent(this, MenuActivity::class.java)
+        intentMenu.putExtra("usuario", txtNombre.text.toString())
+        startActivity(intentMenu)
     }
 }
